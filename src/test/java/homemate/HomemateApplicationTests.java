7408cf60fddab0c4d84d5b74644d7c167686c1fc
githyuniiee <https://github.com/githyuniiee>
2023-10-10 20:03:33 +0900

    gitignore 추가

---
+++ src/test/java/homemate/HomemateApplicationTests.java
@@ -0,0 +1,13 @@
+package homemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomemateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
