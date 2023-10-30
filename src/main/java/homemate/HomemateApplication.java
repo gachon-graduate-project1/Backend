b1659084bc47618c3e1b8e3ca24b000dadfca910
githyuniiee <https://github.com/githyuniiee>
2023-10-30 15:27:27 +0900

    스웨거 /docs 설정

--- src/main/java/homemate/HomemateApplication.java
+++ src/main/java/homemate/HomemateApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
+
+import static org.springframework.web.servlet.function.RequestPredicates.GET;
+import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 @SpringBootApplication
 public class HomemateApplication {
@@ -13,5 +20,12 @@ public class HomemateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomemateApplication.class, args);
 	}
+
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return route(GET("/docs"), req ->
+				ServerResponse.temporaryRedirect(URI.create("swagger-ui/index.html")).build());
+	}
+
 
 }
