932953312319b72a45a533d42f0bfb2a09e9110e
githyuniiee <https://github.com/githyuniiee>
2023-11-03 16:53:45 +0900

    build enum 삭제

--- src/main/java/homemate/constant/BuildingField.java
+++ src/main/java/homemate/constant/BuildingField.java
@@ -1,10 +1,10 @@
-package homemate.constant;
-
-/**
- * 매물 종류
- */
-public enum BuildingField {
-
-    APARTMENT, HOUSE, //.. 의논 후 수정
-
-}
+//package homemate.constant;
+//
+///**
+// * 매물 종류
+// */
+//public enum BuildingField {
+//
+//    APARTMENT, HOUSE, //.. 의논 후 수정
+//
+//}
