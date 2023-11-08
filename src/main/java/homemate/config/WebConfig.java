40b22ccbb2a1725caccdd92a370dc552057dec15
hw130 <hewon130@gmail.com>
2023-11-08 18:49:23 +0900

    포트번호 8080으로 변경

--- src/main/java/homemate/config/WebConfig.java
+++ src/main/java/homemate/config/WebConfig.java
@@ -23,24 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .exposedHeaders(HttpHeaders.LOCATION);
     }
 
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        // images로 오는 요청에 대한 정적 파일 서빙 설정
-////        registry.addResourceHandler("/images/**")
-////                .addResourceLocations("classpath:/images/")
-////                .setCachePeriod(60 * 60 * 24 * 365);
-//        registry.addResourceHandler("/**")
-//                .addResourceLocations("classpath:/static/")
-//                .resourceChain(true)
-//                .addResolver(new PathResourceResolver() {
-//                    @Override
-//                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-//                        Resource requestedResource = location.createRelative(resourcePath);
-//                        return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
-//                                : new ClassPathResource("/static/index.html");
-//                    }
-//                });
-//    }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
